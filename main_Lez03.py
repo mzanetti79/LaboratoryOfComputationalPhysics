4f879c0fbf7490578d4dfada3e9148d3dc6ebcee
MatteoZambra <matteo.zambra@studenti.unipd.it>
2018-10-16 17:47:02 +0200

    manca solo il 6

--- main_Lez03.py
+++ main_Lez03.py
@@ -6,14 +6,15 @@
 
 print("Blyat")
 import time
-
+from math import pi
+from scipy.constants import g
 
 # --------------------------------------------------------------
 
-print("Es 1\n\n")
+print("\n\nEs 1\n")
 # Es 1.1 -------------------------------------------------------
 
-print("Es 1.1\n\n")
+print("Es 1.1\n")
 start = time.time()
 ans = []
 for i in range(3):
@@ -33,7 +34,7 @@ print("List comprehension: \n", ans, "\nin ", finish-start, " s.")
 
 
 # Es 1.2 -----------------------------
-print("Es 1.2\n\n")
+print("\n\nEs 1.2\n")
 
 ans = map(lambda x: x*x , filter(lambda x: x % 2 == 0, range(5)))
 print(list(ans))
@@ -73,7 +74,7 @@ nice try whatsoever
 
 
 # Es 2 ---------------------------------------------
-print("Es 2\n\n")
+print("\n\nEs 2\n")
 
 x = 5
 def f(alist):
@@ -106,7 +107,7 @@ print(adahList)
 
 
 # Es 3 ------------------------------------------------------
-print("Es 3\n\n")
+print("\n\nEs 3\n")
 
 def DecoratorToSayHello(square):
     def wrapper(x):
@@ -133,7 +134,65 @@ print(square(x))
 
 
 # Es 4 -----------------------------------------------------
+print("\n\nEs 4\n")
+
+def factorial(n):
+    if n > 1:
+        ris = n * factorial(n-1)
+        return ris
+    elif n <= 1:
+        return 1
+    #endif
+#enddef
+
+def factorialNoRecursive(n):
+    if n > 1:
+        ris = 1
+        for i in range(1,n+1):
+            ris *= i
+        #enddo
+        return ris
+    elif n <= 1:
+        return 1
+    #endif
+#enddef
+
+n = 4
+start = time.time()
+ris1 = factorial(n)
+finish = time.time()
+print("Fattoriale di ", n, ": ", ris1, " in ", finish-start, " s.")
+
+start = time.time()
+ris2 = factorialNoRecursive(n)
+finish = time.time()
+print("FattNonRicorsivo di ", n, ": ", ris2, " in ", finish-start, " s.")
 
 
+# Es 5 -------------------------------------------------------
+print("\n\nEs 5\n")
 
-        
+print(pi)
+print(g)
+
+densities = {"Al":[0.5,1,2],"Fe":[3,4,5],"Pb": [15,20,30]}
+radii = [1,2,3]
+
+#utilFuncs = [lambda r : 2 * pi * r , lambda r : pi * r**2 , lambda r : 4 * pi * r**3 / 3.0]
+#utilFuncs = [lambda r : 2 * pi * r**i / i (if i == 3 lambda r : 4 * pi * r**i / i)  for i in range(1,4)]
+utilFuncs = [lambda r : c * pi * r**p for c, p in zip([2.0, 1.0, 4.0/3.0],[1, 2, 3])]
+
+
+r = radii[1]
+riss = utilFuncs[0](r)
+print(riss)
+
+for elem in densities.keys():
+    print("Elemento: ", elem)
+    for r in radii:
+        print("Raggio r = ", r)
+        for d,i in zip(densities[elem], utilFuncs):
+            print("Peso del corpo = ", d * i(r))
+        #enddo
+    #enddo
+#enddo
