4b3c0d82d1f0ce0aa89b0e81ad0ee7874c0748ab
eliabntt <eliabntt94@gmail.com>
2019-02-14 10:29:28 +0100

    player substantially done

--- code/strategy.py
+++ code/strategy.py
@@ -11,22 +11,50 @@ class Strategy:
 
 
 class Player(Strategy):
+    M1 = np.array([[2,0],[3,1]])
+    M2 = M1.T
+    
     """Class to describe a player with strategy and history"""
     def __str__(self):
-           return "Player number {} with strategy: {}".format(self.n, self.s)
+           return "Player with strategy: {}".format(self.s)
     
     #the idea is to create player of index n, eventually changing it's strategy
     #and have a list of players inside this class to manage all part of the homework
-    def __init__(self, n, probS = True, k = 0):
+    def __init__(self, probS = True, k = 0):
         if probS:
             self.s = ProbStrategy(k)
         else:
-            self.s = TipForTat()
-        self.n = n
+            self.s = TitForTat()
+        self.stratHist = []
+        self.payoffHist = []
+        self.playedHist = []
     
-    def __get__(self): #Todo Not working
-        return self.s.get()
-    
+    def play(self, sOpponent): 
+        if type(self.s) != TitForTat:
+            action1 = self.s.get()
+        else:
+            action1 = 0 # cooperate
+            if size(sOpponent.playedHist) > 0:
+                action1 = self.s.get(sOpponent.playedHist[-1]) # pass opponent's move
+            
+        if type(sOpponent) != TitForTat:
+            action2 = sOpponent.s.get()
+        else:
+            action2 = 0
+            if size(self.playedHist) > 0:
+                action2 = s2.get(self.playedHist[-1])
+        self.update(action1, action2, False)
+        sOpponent.update(action1, action2, True)
+        
+    def update(self, action1, action2, opponent):
+        self.stratHist.append(str(self.s)) #todo check if better this or k
+        if opponent:
+            self.payoffHist.append(self.M2[action1,action2])
+            self.playedHist.append(action2)
+        else:
+            self.payoffHist.append(self.M1[action1,action2])
+            self.playedHist.append(action1)
+        
 class ProbStrategy(Strategy):
     """Strategy class when probability is used."""
 
