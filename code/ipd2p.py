530d12d4953c9c9acce7130bfac52de063b15971
eliabntt <eliabntt94@gmail.com>
2019-02-17 13:54:57 +0100

    new plot strategy

--- code/ipd2p.py
+++ code/ipd2p.py
@@ -26,7 +26,7 @@ def main():
             p2 = Player(k2)
             p1.play_iter(p2, NUM_ITER)
             print("Evaluating {} - {}...".format(p1.s,p2.s))
-            #print(p1.payoffHist, p1.playedHist)
+            #print(p1.playedHist)
             #print(p2.playedHist)
 
             # plot cumulative rewards
@@ -39,14 +39,14 @@ def main():
             # different dot colors based on action (easiest way to plot points)
             for i in range(0,rew1.size):
                 if p1.playedHist[i] == 0:
-                    plt.plot(i, rew1[i], 'bo')
+                    plt.plot(i, rew1[i], 'bx', markersize=8)
                 else:
-                    plt.plot(i, rew1[i], 'ro')
+                    plt.plot(i, rew1[i], 'rx', markersize=8)
                     
                 if p2.playedHist[i] == 0:
-                    plt.plot(i, rew2[i], 'bo')
+                    plt.plot(i, rew2[i], 'bo', markersize=5)
                 else:
-                    plt.plot(i, rew2[i], 'ro')
+                    plt.plot(i, rew2[i], 'ro', markersize=5)
 
             plt.title("2 pl. game: {} - {}".format(p1.s,p2.s))
             plt.xlabel('Iteration')
@@ -54,17 +54,21 @@ def main():
             # plt.legend(['Player1','Player2'])
             # 0 = cooperate = blue
             plt.legend(handles=[
-                Line2D([0], [0], color='w', marker='_', label='Player 1',
-                          markeredgecolor='b'),
-                Line2D([0], [0], color='w', marker='_', label='Player 2',
-                          markeredgecolor='r'),
-                Line2D([0], [0], color='w', marker='o', label='Defect',
+               # Line2D([0], [0], color='w', marker='_', label='Player 1',
+              #            markeredgecolor='b'),
+             #   Line2D([0], [0], color='w', marker='_', label='Player 2',
+            #              markeredgecolor='r'),
+                Line2D([0], [0], color='w', marker='x', label='P.1 Defect',
+                          markeredgecolor='r'), 
+                Line2D([0], [0], color='w', marker='x', label='P.1 Cooperate',
+                          markeredgecolor='b'),      
+                Line2D([0], [0], color='w', marker='o', label='P.2 Defect',
                           markerfacecolor='r'), 
-                Line2D([0], [0], color='w', marker='o', label='Cooperate',
-                          markerfacecolor='b')          
-            ])
+                Line2D([0], [0], color='w', marker='o', label='P.2 Cooperate',
+                          markerfacecolor='b')
+                ])
 
-            #plt.show()
+            plt.show()
             plt.savefig('../img_v1/idp2p-rewards-{}-{}.png'.format(p1.s,p2.s))
             plt.close()
 
