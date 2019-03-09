d61b9a1d43ad81f1ff03fada8fbdd7c8220c43b7
eliabntt <eliabntt94@gmail.com>
2019-03-09 12:04:08 +0100

    renamed legend for ripdmp with const population fixed legends outside plots saved new figures renamed the file of const population

--- code/ripdmp_const_pop.py
+++ code/ripdmp_const_pop.py
@@ -13,7 +13,7 @@ def main():
     print("Testing repeated round-robin tournament with {}-people".format(NUM_PLAYERS))
 
     k_strategies = Strategy.generatePlayers(NUM_PLAYERS, replace=True)
-    
+
     NUM_REPETITIONS = 0
     repeated_players = []
     strategies_df = pd.DataFrame() # strategies evolution
@@ -21,8 +21,6 @@ def main():
     # while not np.array_equal(k_strategies, np.repeat(k_strategies[0], k_strategies.size)):
     # this is the largest number of elements of a strategy
     while np.unique(k_strategies, return_counts=True)[1].max() < k_strategies.size*3/4:
-        print(k_strategies.size, k_strategies.size*3/4, np.unique(k_strategies, return_counts=True)[1].max())
-        print(k_strategies)
         NUM_REPETITIONS += 1
         players, ranking_df, matches_df = IPDRoundRobin(k_strategies, NUM_ITER) # no strategy change, not against itself
         repeated_players.append(players)
@@ -31,7 +29,7 @@ def main():
         unique, counts = np.unique(k_strategies, return_counts=True)
         df = pd.DataFrame([counts],columns=unique)
         strategies_df = strategies_df.append(df)
-        
+
         # easy fix (depending on task)
         # add one winner strategy or multiple previous winners?
         for i in range(0,int(NUM_PLAYERS * PERCENTAGE)):
@@ -44,16 +42,31 @@ def main():
     print("Convergence speed of round-robin tournament is {} with {}-people".format(NUM_REPETITIONS, NUM_PLAYERS))
 
     # save plots
+    strategies_df = strategies_df.rename(index=str, columns={-3: "TitForTwoTat", -2: "GrimTrigger", -1: "TitForTat",
+                                                                0: "Nice", 100: "Bad", 50: "Indifferent"})
+    for c in strategies_df.columns:
+        if str.isdigit(str(c)):
+            if c > 50:
+                strategies_df = strategies_df.rename(index=str, columns={c: "MainlyBad (k={})".format(c)})
+            else:
+                strategies_df = strategies_df.rename(index=str, columns={c: "MainlyNice (k={})".format(c)})
+
+                
     strategies_df.index = np.arange(strategies_df.index.size)
     strategies_df = strategies_df.fillna(0)
-    strategies_df.plot()
-    plt.legend(ncol=int(NUM_PLAYERS/5))
+    strategies_df.plot(figsize=(12,5))    
+    plt.legend(ncol=int(len(strategies_df.columns)/10), bbox_to_anchor=(1, 1))
     plt.title('Strategies evolution')
     plt.ylabel('Number of strategies')
     plt.xlabel('Time')
-    plt.show()
+    if SAVE_IMG:
+        plt.savefig('../img/ripdmp-evolution-const-pop-{}.png'.format(NUM_PLAYERS))
+        plt.close()
+    else:
+        plt.show()
 
     for (r, players) in zip(np.arange(NUM_REPETITIONS), repeated_players):
+        plt.figure(figsize=(12,5))
         for p in players:
             points = p.get_points()
             plt.plot(points, label=p.s)
@@ -61,12 +74,13 @@ def main():
             plt.xlabel('Match number')
             plt.ylabel('Points')
 
-        plt.legend()
+        plt.legend(ncol=int(NUM_PLAYERS/10), bbox_to_anchor=(1, 1))
+
         if SAVE_IMG:
-            plt.savefig('../img/ripdmp-scores-{}-r{}.png'.format(NUM_PLAYERS, r))
+            plt.savefig('../img/ripdmp-scores-const-pop-{}-r{}.png'.format(NUM_PLAYERS, r))
             plt.close()
         else:
             plt.show()
-
+            
 if __name__ == "__main__":
     main()
