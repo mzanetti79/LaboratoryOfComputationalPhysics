c264160b7ddadd4de181887e3e538f6b9053aa6d
walidmash <waleedmash2@gmail.com>
2019-07-17 18:44:47 +0200

    add stratigies

--- pdFunctions.py
+++ pdFunctions.py
@@ -1,5 +1,6 @@
+import random
 import numpy as np
-from strategies import Nice_guy, Bad_guy
+from strategies import Nice_guy, Bad_guy, Main_nice, Main_bad, Grudger, GoByMajority, Tit_for_tat, TitFor2Tats, SuspiciousTitForTat
 
 ## Iterative Prisoner's Dilemma
 # p1,p2 of type Player
@@ -22,7 +23,14 @@ def createPlayers(playersNames):
     players = []
     stat = {
         'nice guy': (lambda: Nice_guy()),
-        'bad guy': (lambda: Bad_guy())
+        'bad guy': (lambda: Bad_guy()),
+        'main nice': (lambda: Main_nice(random.randint(1, 49))),
+        'main bad': (lambda: Main_bad(random.randint(51, 99) )),
+        'grudger': (lambda: Grudger()),
+        'go by majority': (lambda: GoByMajority()),
+        'tit for tat': (lambda: Tit_for_tat()),
+        'tit for 2 tats': (lambda: TitFor2Tats()),
+        'suspicious tit for tat': (lambda: SuspiciousTitForTat())
     }
     for name in playersNames:
         # check if stratigy name exists    
