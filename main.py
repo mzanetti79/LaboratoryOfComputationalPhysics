986d7d2a12f4c89ad60d2cadc092529e0228e952
hanatj <hanajebril92@gmail.com>
2019-07-17 16:48:19 +0200

    add more strategies

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
 import sys
 sys.path.insert(0, '/strategies.py')
 
-from strategies import Nice_guy, Bad_guy
+from strategies import Nice_guy, Bad_guy, Main_nice, Main_bad, Grudger, GoByMajority
 
 p1 = Nice_guy()
 p2 = Bad_guy()
+p3 = Main_nice(40)
+p4 = Main_bad(70)
+p5 = Grudger()
+p6 = GoByMajority()
+
 p1_score = []
 p2_score = []
+p3_score = []
+p4_score = []
+p5_score = []
+p6_score = []
+
 for i in range(1,10):
     p1_move = p1.play() # 1 or 0
     p2_move = p2.play()
+    p3_move = p3.play()
+    p4_move = p4.play()
+    p5_move = p5.play()
+    p6_move = p6.play()
     p1_score.append(p1.setScore(p1_move, p2_move)) # store in p1 memory and return p1 score of this play
     p2_score.append(p2.setScore(p2_move, p1_move))
-print(p1_score)
+    p3_score.append(p3.setScore(p3_move, p4_move))
+    p4_score.append(p4.setScore(p4_move, p3_move))
+    p5_score.append(p5.setScore(p5_move, p4_move))
+    p6_score.append(p6.setScore(p6_move, p4_move))
+
+print(p3_score)
+print(p4_score)
+print(p5_score)
+print(p6_score)
--- strategies.py
+++ strategies.py
@@ -1,3 +1,5 @@
+import random
+
 class Player(object):
     """A class for a player in the tournament.
     This is an abstract base class, not intended to be used directly.
@@ -43,3 +45,50 @@ class Nice_guy(Player):
 class Bad_guy(Player):
     def play(self):
         return 0
+
+class Main_bad(Player):
+    """randomly defect k\% of the times and cooperate 100-k%, k>50"""
+    def __init__(self,K):
+        Player.__init__(self)
+        self.K = K/10
+
+    def play(self):
+        number = random.randint(1,10)
+        if number >= self.K:
+            return 1
+        else:
+            return 0
+
+class Main_nice(Player):
+    """randomly defect k\% of the times and cooperate 100-k%, k<50"""
+    def __init__(self,K):
+        Player.__init__(self)
+        self.K = K/10
+
+    def play(self):
+        number = random.randint(1,10)
+        if number >= self.K:
+            return 1
+        else:
+            return 0
+
+class Grudger(Player):
+    """A player starts by cooperating however will defect if
+    at any point the opponent has defected."""
+
+    def play(self):
+        if len(self.memory)==0:
+            return 1
+        else:
+            if 0 in self.memory:
+                return 0
+            else:
+                return 1
+
+class GoByMajority(Player):
+    """A player examines the history of the opponent:
+    if the opponent has more defections than cooperations then the player defects."""
+    def play(self):
+        if sum([s == 0 for s in self.memory]) > sum([s == 1 for s in self.memory]):
+            return 0
+        return 1
